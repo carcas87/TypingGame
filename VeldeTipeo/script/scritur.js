562c0d9b9bae8c3b4a73e4c9a31a36961dc0511b
CCastillo <82416268+carcas87@users.noreply.github.com>
2022-12-07 18:09:08 -0600

    v1,2
    dev with problem conection js

---
+++ VeldeTipeo/script/scritur.js
@@ -0,0 +1,69 @@
+// palabras contenidas pre-cargadas
+var listado_palabras = ["CAR", "YELLOW", "BLUE","HTML","FURTHER","ADDRESS","ACTUALLY","REASON","REMAIN",
+"REASON","REMAIN","ESTABLISH","DESCRIBE","DISCOVER","DURING","MEMORY","METHOD","MIND","MOTHER","NECESSARY",
+"RESPONSABILITY","SHOULDER","SOMETIMES","STATEMENT","WINDOWS"];
+
+var acertadas = 0;
+var timer, indice;
+var tiempo = 60;
+
+function comerzarJuego(){
+    //se deja visible la pantalla  y se oculta  la segunda ventana
+    document.getElementById("inicio").style.display = "none";
+    document.getElementById("juego").style.display = "block";
+    document.getElementById("final").style.display = "none";
+
+    //cursor en input
+    document.getElementById("palabra_ingresada").focus();
+    //se carga la palabra
+    cargarPalabra();
+
+    //tiempo - cada segundo al al funcion restar tiempo este es el contador de tiempo
+    timer = setInterval('restarTiempo()',1000);
+
+}
+//----------funcion de carga de palabras random
+function cargarPalabra(){
+    //genera la posicion de forma aleatoria
+    indice = Math.round(Math.random()*(listado_palabras.length-1));
+    document.getElementById("palabra").innerHTML = listado_palabras[
+    indice];
+
+    //se elimina la posicion  ya mostrada
+    listado_palabras.splice(indice,1);
+}
+
+//------funcion que compara la palabra  ingresada con la palabra mostrada
+function comparar(){
+    var palabra_mostrada = document.getElementById("palabra"). innerHTML;
+    var palabra_tecleada = document.getElementById("palabra_ingresada").value;
+//se convierte toda la palabra tecleada a MAYUSC
+palabra_tecleada = palabra_tecleada.toUpperCase();
+
+if(palabra_mostrada==palabra_tecleada){
+    acertadas++;
+    document.getElementById("palabra_ingresada").value="";
+agregarNodo(palabra_tecleada);
+cargarPalabra();  //carga palabra    
+}
+
+}
+function agregarNodo(palabra){
+    var span = document.createElement('span');
+    span.innerHTML = palabra;
+    document.getElementById("registro").appendChild(span);
+
+}
+//-------------------funcion contadora de tiempo
+function restarTiempo(){
+    tiempo --;
+    document.getElementById("tiempo").innerHTML = tiempo;
+    if(tiempo == 0){
+        //se detiene el tiempo
+        clearInterval(timer);
+        document.getElementById("juego").style.display = "none";
+        document.getElementById("final").style.display = "block";
+        document.getElementById("cantidad_fnal").innerHTML = acertadas;
+        
+        }
+}
